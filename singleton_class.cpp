9dd3f5843ab099e7b27de2bf82498aef6e186dec
Kriti <samurai.kittycat@gmail.com>
2022-02-12 15:27:24 +0530

    Update singleton_class.cpp

--- singleton_class.cpp
+++ singleton_class.cpp
@@ -1,39 +1,46 @@
-#include<bits/stdc++.h>
-#include<string.h>
+// Singleton Invoice
+#include <iostream>
+using namespace std;
 
-class singletonClass{
-    private:
-        singletonClass();
-        static singletonClass * instance;
-    public: 
-        static singletonClass* getInstance();
-
+class Singleton {
+public:
+  //Public Static method with return type of Class to access that instance.
+  static Singleton* getInstance();
+private:
+  //Private Constructor
+  Singleton(){
+      
+  };
+  //Private Static Instance of Class
+  static Singleton* objSingleton;
 };
 
-singletonClass * singletonClass::instance = NULL;
+Singleton* Singleton::objSingleton = NULL;
 
-singletonClass * singletonClass::getInstance()
-{
-    if (instance ==NULL){
-        instance = new singletonClass();
-    }
-    return instance;
+Singleton* Singleton::getInstance() {
+  if (objSingleton == NULL) {
+    //Lazy Instantiation: if the instance is not needed it will never be created
+    objSingleton = new Singleton();
+    cout << "Object is created" << endl;
+  }
+  else
+  {
+    cout << "Object is already created" << endl;
+  }
+  return objSingleton;
 }
 
-singletonClass::singletonClass(){
-
-}
-
-int main(){
-    singletonClass* a = singletonClass::getInstance();
-    singletonClass* b = singletonClass::getInstance();
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
+int main() {
+  Singleton::getInstance();
+  Singleton::getInstance();
+  Singleton::getInstance();
+  return 0;
 }
 
 
 // If comes out to be same address then singleton class maintained
 // output
-// 0x558feb005eb0
-// 0x558feb005eb0
+// Object is created
+// Object is already created
+// Object is already created
 
